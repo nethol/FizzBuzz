d1f995f5470f420de8dc21fc8e56f5c565ae3c09
Ernesto Pedraza <ernestopedraza@MacBook-Pro-de-Ernesto.local>
2017-06-14 18:47:54 -0700

    Initial add

---
+++ FizzBuzz.Library/FizzBuzzService.cs
@@ -0,0 +1,31 @@
+namespace FizzBuzz.Library
+{
+    public class FizzBuzzService
+    {
+        public string Print(int n)
+        {
+            if(IsDivisibleByFive(n) && IsDivisibleByThree(n))
+            {
+                return "FizzBuzz";
+            }else if(IsDivisibleByFive(n)){
+                return "Buzz";
+            }else if(IsDivisibleByThree(n))
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return n.ToString();
+            }
+            
+        }
+        private bool IsDivisibleByThree(int n)
+        {
+            return n % 3 == 0;
+        }
+        private bool IsDivisibleByFive(int n)
+        {
+            return n % 5 == 0;
+        }
+    }
+}
---
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz.Library;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        private FizzBuzzService _fizzBuzz;
+
+        public FizzBuzzTests()
+        {
+            _fizzBuzz = new FizzBuzzService();
+        }
+        [TestMethod]
+        public void ShouldPrintNumber()
+        {
+            Assert.AreEqual("1", _fizzBuzz.Print(1));
+        }
+
+        [TestMethod]
+        public void ShouldPrintFizz()
+        {
+            Assert.AreEqual("Fizz", _fizzBuzz.Print(3));
+        }
+        [TestMethod]
+        public void ShouldPrintBuzz()
+        {
+            Assert.AreEqual("Buzz", _fizzBuzz.Print(5));
+            Assert.AreEqual("Buzz", _fizzBuzz.Print(10));
+        }
+
+        [TestMethod]
+        public void ShouldPrintFizzBuzz()
+        {
+            Assert.AreEqual("FizzBuzz", _fizzBuzz.Print(15));
+            Assert.AreEqual("FizzBuzz", _fizzBuzz.Print(30));
+        }
+    }
+}
